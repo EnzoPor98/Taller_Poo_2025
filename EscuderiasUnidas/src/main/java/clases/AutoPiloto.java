cc24f74bb88449b1052523486f048a932cc48a1f
EnzoPor98 <127218831+EnzoPor98@users.noreply.github.com>
2025-10-17 14:04:05 -0300

    RelacionAutoPiloto
    Se relacionó la clase AutoPiloto con la clase Carrera.

--- EscuderiasUnidas/src/main/java/clases/AutoPiloto.java
+++ EscuderiasUnidas/src/main/java/clases/AutoPiloto.java
@@ -1,21 +1,35 @@
 package clases;
 
+import java.util.ArrayList;
+
 public class AutoPiloto {
     private String fechaAsignacion;
     private Auto auto;
     private Piloto piloto;
+    private ArrayList<Carrera> carreras;
 
     // CONSTRUCTORES.
     public AutoPiloto() {
         fechaAsignacion = "";
         auto = null;
         piloto = null;
+        carreras = new ArrayList<Carrera>();
     }
 
-    public AutoPiloto(String fechaAsignacion, Auto auto, Piloto piloto) {
+    public AutoPiloto(String fechaAsignacion, Auto auto, Piloto piloto, ArrayList<Carrera> carreras) {
         this.fechaAsignacion = fechaAsignacion;
         this.auto = auto;
         this.piloto = piloto;
+        this.carreras = carreras;
+    }
+
+    // LISTA CARRERAS.
+    public void agregarCarrera(Carrera c) {
+        carreras.add(c);
+    }
+
+    public void borrarCarrera(Carrera c) {
+        carreras.remove(c);
     }
 
     // GETTERS & SETTERS.
