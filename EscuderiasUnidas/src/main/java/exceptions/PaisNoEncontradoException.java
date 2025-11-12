719b68119ad11c901400e4dde21622aa1234a385
Ezee-79 <gezromerosuju@gmail.com>
2025-11-12 20:54:27 -0300

    Modificacion clase VentanaAuto, agregacion y creacion de nuevas Excepciones
    Modificacion e implementacion de excepciones en clase VentanaAuto, agregacion y creacion de nuevas Excepciones

---
+++ EscuderiasUnidas/src/main/java/exceptions/PaisNoEncontradoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+/**
+ *
+ * @author Gaston PC
+ */
+public class PaisNoEncontradoException extends Exception {
+    public PaisNoEncontradoException (String msj){
+        super(msj);
+    }
+}
